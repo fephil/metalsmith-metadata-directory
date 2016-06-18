b01726f01887efa9deef33a3fa529b1d17c6eebe
Phil Lennon <phil@frontendphil.com>
2016-06-18 18:22:04 +0100

    error if json is malformed, thanks to @grumpydev22 for unit test help

--- test/index.js
+++ test/index.js
@@ -1,15 +1,25 @@
-var expect = require('chai').expect
+var should = require('chai').should()
 var Metalsmith = require('metalsmith')
 var metadata = require('..')
+
+it('should error if the JSON file is malformed', function (done) {
+  var metalsmith = Metalsmith('test/fixtures/json-malformed').use(metadata({ directory: 'test/fixtures/json-malformed/src/**/*.json' }))
+  metalsmith.build(function(err) {
+    errMessage = String(err);
+    errMessage.should.equal('Error: Malformed data in example.json')
+    done() // don't return the error to metalsmith
+  })
+})
 
 it('should read JSON and put into metalsmith.metadata()', function (done) {
   var metalsmith = Metalsmith('test/fixtures/json').use(metadata({ directory: 'test/fixtures/json/src/**/*.json' }))
   metalsmith.build(function(err) {
+    metalsmith.metadata().should.deep.equal({ example: { text: 'Text from a json file' } })
+
     if (err) {
       return done(err)
     }
 
-    expect(metalsmith.metadata()).to.deep.equal({ example: { text: 'Text from a json file' } })
     done()
   })
 })
