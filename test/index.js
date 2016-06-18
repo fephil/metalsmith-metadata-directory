6b8e0d09d4b82c25b76f736be7a334b3eedeb95d
Phil Lennon <phil@frontendphil.com>
2016-06-18 18:40:30 +0100

    ignore a json file if it is empty, fixes #11

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var should = require('chai').should()
 var Metalsmith = require('metalsmith')
-var metadata = require('..')
+var metadata = require('../lib/index.js')
 
 it('should error if the JSON file is malformed', function (done) {
   var metalsmith = Metalsmith('test/fixtures/json-malformed').use(metadata({ directory: 'test/fixtures/json-malformed/src/**/*.json' }))
@@ -10,6 +10,19 @@ it('should error if the JSON file is malformed', function (done) {
     errMessage.should.equal('Error: Malformed data in example.json')
 
     done() // don't return the error to metalsmith
+  })
+})
+
+it('should ignore a JSON file if is empty', function (done) {
+  var metalsmith = Metalsmith('test/fixtures/json-empty').use(metadata({ directory: 'test/fixtures/json-empty/src/**/*.json' }))
+  metalsmith.build(function(err) {
+    metalsmith.metadata().should.deep.equal({ example: { text: 'Text from a json file' } })
+
+    if (err) {
+      return done(err)
+    }
+
+    done()
   })
 })
 
