865de6e1f0549fd88162e9127e2f123cd545f844
Phil Lennon <phil@frontendphil.com>
2016-06-18 16:51:05 +0100

    add basic json parse test

--- test/index.js
+++ test/index.js
@@ -1,1 +1,15 @@
-/* tba */
+var expect = require('chai').expect
+var Metalsmith = require('metalsmith')
+var metadata = require('..')
+
+it('should read JSON and put into metalsmith.metadata()', function (done) {
+  var metalsmith = Metalsmith('test/fixtures/json').use(metadata({ directory: 'test/fixtures/json/src/**/*.json' }))
+  metalsmith.build(function(err) {
+    if (err) {
+      return done(err)
+    }
+
+    expect(metalsmith.metadata()).to.deep.equal({ example: { text: 'Text from a json file' } })
+    done()
+  })
+})
